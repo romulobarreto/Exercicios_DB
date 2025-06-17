d11286491031eb40310da0c8d6b0815c6f195a19
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-06-17 13:17:58 -0300

    chore: adiciona a funcao que cria o banco

---
+++ sql/database/database.py
@@ -0,0 +1,12 @@
+import sqlite3 
+
+def criar_banco():
+    with sqlite3.connect('system.db') as conexao:
+        cursor = conexao.cursor()
+        cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRYMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    email TEXT,
+    senha TEXT)
+''')
